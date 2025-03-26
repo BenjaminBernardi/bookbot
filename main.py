79892079fcf96940c3e97cd36cb4fc121a4a321f
benjaminbernardi <benjamin.bernardi@outlook.com>
2025-03-26 11:41:20 +0100

    final version

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path_to_book = sys.argv[1]
+
 from stats import get_number_of_words
 from stats import get_number_of_characters
+from stats import sort_chars_by_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,10 +16,23 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_text = get_book_text("/home/aemulus/projects/bookbot/books/frankenstein.txt")
+    book_text = get_book_text(path_to_book)
     number_words = get_number_of_words(book_text)
     number_characters = get_number_of_characters(book_text)
-    print(f"{number_words} words found in the document")
-    print(number_characters)
+    sort_characters = sort_chars_by_count(number_characters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}")
+    print("----------- Word Count ----------")
+    print(f"Found {number_words} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sort_characters:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,17 @@ def get_number_of_characters(text):
             list[character] = 1
     
     return list
+
+def sort_chars_by_count(char_dict):
+    char_list = []
+
+    for char, count in char_dict.items():
+        char_info = {"char": char, "count": count}
+        char_list.append(char_info)
+
+    def sort_on(dict):
+        return dict["count"]
+
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list   
